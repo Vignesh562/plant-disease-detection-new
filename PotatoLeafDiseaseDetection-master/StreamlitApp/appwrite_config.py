ae81b7e747c4771a1b3c45de53f9fd7ed6d7dee0
Vignesh Parmar <122289003+Vignesh562@users.noreply.github.com>
2025-07-30 19:19:13 +0530

    Update appwrite_config.py

--- PotatoLeafDiseaseDetection-master/StreamlitApp/appwrite_config.py
+++ PotatoLeafDiseaseDetection-master/StreamlitApp/appwrite_config.py
@@ -6,34 +6,13 @@ from appwrite.services.account import Account
 # Initialize Appwrite client
 client = Client()
 
-client.set_endpoint("https://fra.cloud.appwrite.io/v1")  # Do not change
-client.set_project("688a1b610038ca502d2f")  # Replace with your real project ID
+client.set_endpoint("https://cloud.appwrite.io/v1")  # Do not change
+client.set_project("your_actual_project_id")  # Replace with your real project ID
+client.set_key("your_api_key")  # Secure key for server access (will guide on creating)
 
 # Setup services
 account = Account(client)
-
-# To create a user
-account.create(email="email@example.com", password="securepassword")
-
-# To login
-account.create_email_session(email="email@example.com", password="securepassword")
-
-#Database 
-databases = Databases(client)
-
-databases.create_document(
-    database_id="your-db-id",
-    collection_id="predictions",
-    document_id="unique()",
-    data={
-        "user_email": email,
-        "image_url": image_url,
-        "predicted_disease": disease,
-        "description": disease_desc,
-        "timestamp": datetime.now().isoformat()
-    }
-)
-
+database = Databases(client)
 
 # Export for other files to use
 __all__ = ["client", "account", "database"]
